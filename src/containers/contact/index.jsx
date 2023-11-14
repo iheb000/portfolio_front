35fa04370b979d25355b73745532a7bd9922f754
iheb <113391637+iheb000@users.noreply.github.com>
2023-11-15 00:49:20 +0100

    Update index.jsx
    appi

--- src/containers/contact/index.jsx
+++ src/containers/contact/index.jsx
@@ -23,7 +23,7 @@ const Contact = () => {
 
     try {
       //"http://localhost:5000/api/submit"
-      const response = await fetch("https://iheb-ben-soltane.onrender.com/contact/api/submit", {
+      const response = await fetch("https://iheb-ben-soltane-server.onrender.com/api/submit", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
